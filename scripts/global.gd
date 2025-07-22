daa9a13c6829c008c898b7862fcc0b2adfdee09c
yrieth <misha525573@gmail.com>
2025-07-22 13:38:16 +0200

    added basic saving to file

--- scripts/global.gd
+++ scripts/global.gd
@@ -30,6 +30,7 @@ const COLLECTION = [
 	}
 ]
 
+var unlockedCards: Dictionary[String, bool]
 @onready var isServer: bool = false
 @onready var isClient: bool = false
 @onready var port: int = 25565
@@ -37,3 +38,15 @@ const COLLECTION = [
 var peerID: int
 var gameNode: Control
 @onready var yourTurn: bool = false
+
+func _ready() -> void:
+	var unlockedCardsFile: FileAccess = FileAccess.open("res://saves/unlockedCards.save", FileAccess.READ)
+	if unlockedCardsFile == null or !unlockedCardsFile.is_open():
+		unlockedCardsFile = FileAccess.open("res://saves/unlockedCards.save", FileAccess.WRITE)
+		for i in COLLECTION:
+			unlockedCards.get_or_add(i.Name, false)
+		unlockedCardsFile.store_var(unlockedCards)
+		unlockedCardsFile.close()
+	unlockedCardsFile = FileAccess.open("res://saves/unlockedCards.save", FileAccess.READ)
+	unlockedCards = unlockedCardsFile.get_var()
+	print(unlockedCards)
